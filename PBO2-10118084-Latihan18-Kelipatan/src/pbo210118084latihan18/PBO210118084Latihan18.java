978208af1597a6078100538be7fff88776877bbe
DIVI ADIFFIA FREZA ALANA <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-10-09 04:00:59 -0600

    Add files via upload

---
+++ PBO2-10118084-Latihan18-Kelipatan/src/pbo210118084latihan18/PBO210118084Latihan18.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210118084latihan18;
+
+/**
+ *
+ * @author
+ * Nama  : Divi Adiffia Freza Alana
+ * Kelas : PBO2
+ * Nim   : 10118084
+ * Deskripsi Program : Program ini berisi program untuk menampilkan kelipatan
+
+ */
+public class PBO210118084Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      for (double i = 1; i <= 10; i++) {
+       System.out.println(3.5*i);
+      }
+
+           
+       }
+        
+    }
+    
+
